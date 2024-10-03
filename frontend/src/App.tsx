4c32faaf7cefa25592af5758836549b3da2000e2
Zaiqin <phuazaiqin@gmail.com>
2024-10-03 18:14:21 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -338,6 +338,7 @@ const App: React.FC = () => {
       );
       const data = await response.json();
       setTags(data);
+      setLoadedParts((prev) => ({ ...prev, tags: true }));
     } catch (error) {
       console.error('Error fetching tags:', error);
     }
@@ -423,14 +424,24 @@ const App: React.FC = () => {
         )}
       </div>
 
-      {!isLoggedIn ? (
-        <div className="flex flex-col items-center justify-center pt-[30vh]">
+      {!isLoggedIn && !(loadedParts.finances && loadedParts.categories && loadedParts.presets && loadedParts.tags) ? (
+        <div className="flex flex-col items-center justify-center pt-[15vh]">
           <h2 className="text-xl sm:text-2xl font-semibold text-gray-700 mb-4">
             Welcome to Finance.io
           </h2>
-          <p className="text-gray-600 mb-6 text-center">
-            Please log in to start managing your finances.
-          </p>
+            <p className="text-lg text-gray-600 mb-6 text-center max-w-md w-2/3">
+            Finance.io is your personal finance manager. Easily track your income and expenses, categorize transactions, and gain insights into your financial health. Start managing your finances efficiently with Finance.io.
+            </p>
+          <div className="text-gray-600 mb-6 text-center max-w-md">
+            <h3 className="text-md font-semibold text-gray-700 mb-2">Built with Modern Technologies</h3>
+            <ul className="list-disc list-inside text-left mx-auto text-sm">
+              <li>React, Typescript for building dynamic user interfaces</li>
+              <li>TailwindCSS for modern, responsive styling</li>
+              <li>Node.js and Express.js for robust backend services</li>
+              <li>MongoDB for efficient data storage and retrieval</li>
+              <li>Vercel for seamless deployment and hosting</li>
+            </ul>
+          </div>
           {isLoading && (
             <div className="flex items-center justify-center">
               <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 border-t-blue-500 h-10 w-10 mb-4 animate-spin"></div>
